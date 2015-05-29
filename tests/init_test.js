bc0355e7ce5b7e29a00732e78226c9a363087712
katjam <katja@neontribe.co.uk>
2015-05-29 09:25:11 +0100

    Add check for My Assessment menu link

--- tests/init_test.js
+++ tests/init_test.js
@@ -1,10 +1,10 @@
 var url = 'http://localhost:9001/build';
-casper.test.begin('Title page', 2, function suite(test) {
+casper.test.begin('Title page', 3, function suite(test) {
     casper.start(url, function() {
       this.test.comment(this.getCurrentUrl());
       this.test.assertHttpStatus(200, 'SEAP is up');
       test.assertTitle("My PIP Assessment", "PIP page title is the one expected");
-      //test.assertExists('button[data-action="about-pip"]', "All about PIP button found");
+      test.assertExists('a[data-action="stats"]', "My assessment menu link found");
     });
 
     casper.run(function() {
