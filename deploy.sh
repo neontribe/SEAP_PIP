457132422743357c2aff74a2e180bc10563e6320
katjam <katja@neontribe.co.uk>
2015-08-03 14:27:36 +0100

    try encoded git pass

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
 #checkout gh_pages branch and update with contents of build folder
 git remote rm origin
-git remote add origin https://katjam:9e0e2d43e5b1c07ee6da44d25e46840795da1e9f@github.com/neontribe/SEAP_PIP.git
+git remote add origin https://katjam:$GIT_PASS@github.com/neontribe/SEAP_PIP.git
 git fetch
 git checkout gh-pages
 cp -r build/* .
@@ -27,14 +27,11 @@ fi
 if [ "$TRAVIS_TAG" ]; then
     echo -e "Release tag:"
     echo -e $TRAVIS_TAG
-    #Deploy gh-pages to live server
-    #todo
+    #Deploy gh-pages to live server todo LIVE details to be added
     cd ..
     tar -czf release.tgz SEAP_PIP 
     sudo apt-get -y install sshpass
-    set -x
-    ls
     sshpass -p $DEPLOY_PASS scp -o stricthostkeychecking=no release.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-    sshpass -p $DEPLOY_PASS ssh -o stricthostkeychecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/release_deploy.sh
+    sshpass -p $DEPLOY_PASS ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/release_deploy.sh
     echo -e "Deploy successful."
 fi
