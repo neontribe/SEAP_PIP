9ca8a275c171bbb279a3333c9f619ee79f07410b
katjam <katja@neontribe.co.uk>
2015-08-24 10:13:34 +0100

    Test complete live deploy

--- deploy-seap-pip.sh
+++ deploy-seap-pip.sh
@@ -2,6 +2,9 @@
 echo -e "Tests passed - deploying to LIVE"
 
 #Deploy gh-pages release tarball to live server
-scp -r -i .ssh/nt_seap_deploy testdir/* seaproot@94.142.89.2:/var/www/pip-assessment
+scp -r -i .ssh/nt_seap_deploy SEAP_PIP/* seaproot@94.142.89.2:/var/www/pip-assessment
 
 echo -e "Deploy successful."
+
+echo -e "Removing build files"
+rm -rf SEAP_PIP release.tgz
