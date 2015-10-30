386e32e4ed6bd801568c451bb47e7f4d241fee32
tomltd <tomfreestone@gmail.com>
2015-10-30 15:17:24 +0000

    add generate task to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function(grunt){
       }
     });
 
-    grunt.registerTask('test', ['clean:initial', 'copy', 'compile-handlebars', 'bake','htmlhint', 'jshint', 'concat', 'autoprefixer', 'clean:tidyup', 'connect', 'casperjs']);
+    grunt.registerTask('test', ['generate', 'connect', 'casperjs']);
     grunt.registerTask('generate', ['clean:initial', 'copy','compile-handlebars','bake', 'htmlhint', 'jshint', 'concat', 'autoprefixer', 'clean:tidyup']);
     grunt.registerTask('generate-production', ['clean:initial','compile-handlebars', 'htmlhint', 'jshint', 'copy', 'concat', 'uglify', 'autoprefixer','cssmin', 'clean:tidyup']);
 };
