03d7a6798f961e744cd0c09ea87f6a351c749656
katjam <katja@neontribe.co.uk>
2015-11-13 11:30:37 +0000

    Travis build 407 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14134,6 +14134,16 @@ if (db.isEmpty('pipAss')) {
 
 }
 
+// check if mobile device and if so closes all expanded lists
+$(function() {
+   var isMobile = window.matchMedia("only screen and (max-width: 800px)");
+
+   if (isMobile.matches) {
+       $("#about-PIP .expandies button").attr("aria-expanded", "false");
+       $("#about-PIP .expandies h2").next().attr("aria-hidden", "true");
+   }
+});
+
 /**********************************************************************
 FUNCTIONS
 **********************************************************************/
@@ -14228,6 +14238,10 @@ function loadSlide(id, type) {
 
   if (id === 'categories') {
     compileCategories();
+  }
+
+  if (id === 'about-PIP') {
+    compileAboutButtons();
   }
 
   if (id === 'category-finished') {
@@ -14590,6 +14604,13 @@ function compileStats() {
   var output = template(pipAssData);
   $('#stats-content').html(output);
 
+}
+
+function compileAboutButtons() {
+  var template = Handlebars.compile(document.getElementById("about-buttons-template").innerHTML);
+  var pipAssData = db.get('pipAss');
+  var output = template(pipAssData);
+  $('.expandies.information .about-buttons-content').html(output);
 }
 
 function compileCategories() {
