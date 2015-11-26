f42405869fb8ef2e476a22ad03351088a3bf95ea
katjam <katja@neontribe.co.uk>
2015-11-26 10:39:23 +0000

    Travis build 548 pushed to Github Pages

--- js/all-scripts.js
+++ js/all-scripts.js
@@ -14517,7 +14517,6 @@ function tally() {
 function qualify(points) {
 
   var total = tally();
-  console.log(total);
   if (total.mobility >= 8) {
 
     //don't show the slide if you have already
@@ -14578,7 +14577,6 @@ function qualify(points) {
 
   }
 
-  console.log(db.get('pipAss'));
 }
 
 // helper function to test numeric strings
